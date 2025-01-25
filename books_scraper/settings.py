24755f13affebc54906ba3e1117b5689c7874b82
choi <gg78700@gmail.com>
2025-01-25 22:31:05 +0900

    change settings and make base craling code

--- books_scraper/settings.py
+++ books_scraper/settings.py
@@ -11,7 +11,7 @@ BOT_NAME = "books_scraper"
 
 SPIDER_MODULES = ["books_scraper.spiders"]
 NEWSPIDER_MODULE = "books_scraper.spiders"
-
+FEED_EXPORT_ENCODING = "utf-8"
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = "books_scraper (+http://www.yourdomain.com)"
--- books_scraper/spiders/books.py
+++ books_scraper/spiders/books.py
@@ -7,4 +7,28 @@ class BooksSpider(scrapy.Spider):
     start_urls = ["https://books.toscrape.com/"]
 
     def parse(self, response):
-        pass
+        books = response.css("ol.row li")
+        count_check = 0  # 페이지 내에서 처리한 항목 수를 카운트
+
+        for book in books:
+            title = book.css("h3 > a::attr(title)").get()
+            price = book.css("p.price_color::text").get().replace("£", "")
+            
+            # 별점 클래스에서 숫자 변환 처리
+            rating_class = book.css("p.star-rating::attr(class)").get()
+            if "One" in rating_class:
+                int_rating = 1
+            elif "Two" in rating_class:
+                int_rating = 2
+            elif "Three" in rating_class:
+                int_rating = 3
+            elif "Four" in rating_class:
+                int_rating = 4
+            elif "Five" in rating_class:
+                int_rating = 5
+            else:
+                int_rating = None
+
+            in_stock = "Y" if "In stock" in book.css("p.instock.availability::text").get() else "N"
+            category = response.css("ul.breadcrumb li:nth-child(3) > a::text").get()
+            image_url = book.css("img::attr(src)").get()
